2f41be8255b57849629030d294d23e0737de4b8d
Estefania <estefaniadiaz1110@gmail.com>
2023-09-21 18:28:04 -0500

    Reporte de Usuarios en Excel

--- src/main/java/com/modisteria/dl/util/ReporteUsuariosEXCEL.java
+++ src/main/java/com/modisteria/dl/util/ReporteUsuariosEXCEL.java
@@ -10,30 +10,30 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
 
-import com.modisteria.dl.model.Citas;
+import com.modisteria.dl.model.Usuario;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 //Siempre debemos indicar que este archivo es un componente de Spring boot con la siguiente anotación
-@Component("form.xlsx") //No puede tener la misma ruta que en el reporte de pdf entonces debemos adicionar el formto en el que vamos a trabajar ahora que es .xlsx
-public class ReporteCitasEXCEL extends AbstractXlsxView {
+@Component("vistausuario.xlsx") //No puede tener la misma ruta que en el reporte de pdf entonces debemos adicionar el formto en el que vamos a trabajar ahora que es .xlsx
+public class ReporteUsuariosEXCEL extends AbstractXlsxView {
 	@Override
 	//Este método contiene todos los argumentos necesarios para crear un documento de excel
 	public void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		
 		response.setHeader("Content-Disposition", "attachment; filename=\"listado-citas.xlsx\"");
-		Sheet hoja = workbook.createSheet("Citas");
+		Sheet hoja = workbook.createSheet("Usuarios");
 		
 		
 		Row filaTitulo = hoja.createRow(0); //Dento del paréntesis va el indice de la fila donde coloco la información
 		Cell celda = filaTitulo.createCell(0); //Como iniciamos desde la columna A en excel el indice es 0
-		celda.setCellValue("LISTADO GENERAL DE CITAS");
+		celda.setCellValue("LISTADO GENERAL DE USUARIOS");
 		
 		Row filaData = hoja.createRow(2);
 		
 		//Creamos un arreglo para indicar los titulos de cada columna
-		String[] columnas = {"ID", "FECHA", "OBJETIVO", "IMAGEN", "USUARIO", "ESTADO"};
+		String[] columnas = {"ID", "NOMBRE", "CORREO", "TELÉFONO", "CONSTRASEÑA", "ROL"};
 		
 		//Creamos un bucle for para que aparezcan los titulos guardados en el arreglo
 		for (int i = 0; i < columnas.length; i++) {
@@ -41,20 +41,20 @@ public class ReporteCitasEXCEL extends AbstractXlsxView {
 			celda.setCellValue(columnas[i]);
 		}
 		
-		//Creamos la lista para empezar el proceso de traer la información de las citas
-		List<Citas> listaCitas = (List<Citas>) model.get("citas");
+		//Creamos la lista para empezar el proceso de traer la información de los usuarios
+		List<Usuario> listaUsuarios = (List<Usuario>) model.get("usuario");
 		
 		//Creamos un foreach para recorrer cada registro
-		int numFila = 3; //Indice de la fila en la que vamos a empezar a poner los datos de las citas
-		for (Citas citas : listaCitas) {
+		int numFila = 3; //Indice de la fila en la que vamos a empezar a poner los datos de los usuarios
+		for (Usuario usuario : listaUsuarios) {
 			filaData = hoja.createRow(numFila);
 			
-			filaData.createCell(0).setCellValue(citas.getId());
-			filaData.createCell(1).setCellValue(citas.getFecha().toString());
-			filaData.createCell(2).setCellValue(citas.getObjetivo());
-			filaData.createCell(3).setCellValue(citas.getImagen());
-			filaData.createCell(4).setCellValue(citas.getNombre_usuario());
-			filaData.createCell(5).setCellValue(citas.getEstado().getNombre_estado());
+			filaData.createCell(0).setCellValue((String.valueOf(usuario.getId())));
+			filaData.createCell(1).setCellValue(usuario.getNombre_completo());
+			filaData.createCell(2).setCellValue(usuario.getCorreo());
+			filaData.createCell(3).setCellValue(usuario.getTelefono());
+			filaData.createCell(4).setCellValue(usuario.getPassword());
+			filaData.createCell(5).setCellValue(usuario.getRol().getNombre());
 			
 			numFila ++;
 		}
