a0b2fc96fd2177626f7b94c0d370a7ee833cf722
Juano <juanjosemunozva@gmail.com>
2023-09-22 09:22:16 -0500

    Agregar vista estadoS

--- src/main/java/com/modisteria/dl/controller/EstadoController.java
+++ src/main/java/com/modisteria/dl/controller/EstadoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,32 +27,34 @@ public class EstadoController {
 	public String listar(Model model) {
 		List<Estado> estado = service.listar();
 		model.addAttribute("estados", estado);
+		model.addAttribute("estado", new Estado());
+		return "estados";
+	}
+	@GetMapping("/listarEstados/{id}")
+	public String estadoId(@PathVariable int id, Model model, @RequestParam(name = "action") String action) {
+		
+		List<Estado> estado = service.listar();
+		model.addAttribute("estados", estado);
+
+		model.addAttribute("estado", new Estado());
+
+		Optional<Estado> estadoE = service.listarId(id);
+		model.addAttribute("estado", estadoE.get());
+		
+		boolean borrarEstado = ("borrar".equals(action));
+		boolean editEstado = ("edit".equals(action));
+        model.addAttribute("editEstado", editEstado);
+        model.addAttribute("borrarEstado", borrarEstado);
 		return "estados";
 	}
 	
-	@GetMapping("/nuevoEstado")
-	public String agregar(Model model) {
-		model.addAttribute("estado", new Estado());
-		return "nuevoEstado";
-	}
 	
+
 	@PostMapping("/guardarEstado")
 	public String guardar(@Valid Estado e) {
 		service.guardar(e);
 		return "redirect:/listarEstados";
 	}
-	
-	
-	@GetMapping("/editarEstado/{id}")
-    public String editar(@PathVariable int id, Model model) {
-        Optional<Estado> estado = service.listarId(id);
-        if (estado.isPresent()) {
-            model.addAttribute("estado", estado.get());
-            return "editarEstado";
-        } else {
-            return "redirect:/listarEstados";
-        }
-    }
 	
 	@GetMapping("/eliminarEstado/{id}")
 	public String eliminar(@PathVariable int id) {
