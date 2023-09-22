b22a1959e2e52cc53f2b0b8912365ef773b21b8e
Yarce <camiloyarce11@gmail.com>
2023-09-22 09:27:48 -0500

    editar usuarios listo

--- src/main/java/com/modisteria/dl/controller/AdminController.java
+++ src/main/java/com/modisteria/dl/controller/AdminController.java
@@ -65,12 +65,8 @@ public class AdminController {
         model.addAttribute("borrarCita", borrarCita);
         return "usuarios";
     }
-    
-    @PostMapping("/userAdd")
-    public String registrarUsuario(@ModelAttribute("nuevoUsuario") Usuario usuario) {
-        usuarioRepositorio.save(usuario);
-        return "redirect:/usuarios";
-    }
+
+
     @PostMapping("/userEdit")
     public String editarUsuario( Model model, @RequestParam("id") Long id, @Validated Usuario usuarioForm ) {
         Usuario usuarioE = usuarioService.listarId(id);
@@ -83,6 +79,17 @@ public class AdminController {
         usuarioRepositorio.save(usuarioE);
          return "redirect:/usuarios/";
      }
+    @PostMapping("/userAdd")
+    public String registrarUsuario(@ModelAttribute("nuevoUsuario") Usuario usuario) {
+        usuarioRepositorio.save(usuario);
+        return "redirect:/usuarios/";
+    }
+    @PostMapping("/eliminarUsuario")
+    public String eliminarUsuario(@RequestParam("id") Long id) {
+        usuarioRepositorio.deleteById(id);
+        return "redirect:/usuarios/";
+    }
 
-    
+
+
 }
